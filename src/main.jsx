19253041a6b08abd029531d0f12bdc78a1783025
peachfruittea <chocotin1624@gmail.com>
2026-02-04 13:13:45 +0800

    BrowserRouter改為HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import App from "./App.jsx";
 import "./index.css";
 
-const basename = import.meta.env.PROD ? "/react-weekly-mission" : "/";
-
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={basename}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>,
 );
